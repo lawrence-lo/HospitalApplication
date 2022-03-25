cf0e7f48b9dfb998eb5bf3eb845ea9f56fd9d3c4
Lawrence Lo <lolung@gmail.com>
2022-03-25 01:00:21 -0400

    add-migration posts

--- HospitalApplication/Models/IdentityModels.cs
+++ HospitalApplication/Models/IdentityModels.cs
@@ -25,6 +25,8 @@ namespace HospitalApplication.Models
         {
         }
 
+        public DbSet<Post> Posts { get; set; }
+
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
---
+++ HospitalApplication/Models/Post.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HospitalApplication.Models
+{
+    public class Post
+    {
+        [Key]
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string Content { get; set; }
+        
+        //A post belongs to one user
+        //A user can create many posts
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+        public virtual ApplicationUser User { get; set; }
+    }
+}
