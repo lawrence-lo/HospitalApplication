7f1de683a38c15816510e9c6f4a0afbd2fb42fbf
nayer-rsl <nayer.rsl@gmail.com>
2022-03-30 12:42:03 -0400

    Update Donor

--- HospitalApplication/Models/Donor.cs
+++ HospitalApplication/Models/Donor.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace HospitalApplication.Models
 {
     public class Donor
+    {
+        [Key]
+        public int DonorID { get; set; }
+        public string DonorName { get; set; }
+        public string DonorEmail { get; set; }
+        public string DonorAddress { get; set; }
+        public string DonorPhone { get; set; }
+
+
+    }
+    public class DonorDto
     {
         public int DonorID { get; set; }
         public string DonorName { get; set; }
