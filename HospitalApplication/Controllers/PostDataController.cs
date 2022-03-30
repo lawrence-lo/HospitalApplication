37724fc175d85ea212aa0af18555876d02b22228
Lawrence Lo <lolung@gmail.com>
2022-03-29 22:43:00 -0400

    Update posts ForeignKey

---
+++ HospitalApplication/Controllers/PostDataController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using HospitalApplication.Models;
+
+namespace HospitalApplication.Controllers
+{
+    public class PostDataController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/PostData
+        public IQueryable<Post> GetPosts()
+        {
+            return db.Posts;
+        }
+
+        // GET: api/PostData/5
+        [ResponseType(typeof(Post))]
+        public IHttpActionResult GetPost(int id)
+        {
+            Post post = db.Posts.Find(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(post);
+        }
+
+        // PUT: api/PostData/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutPost(int id, Post post)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != post.PostID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(post).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PostExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/PostData
+        [ResponseType(typeof(Post))]
+        public IHttpActionResult PostPost(Post post)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Posts.Add(post);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = post.PostID }, post);
+        }
+
+        // DELETE: api/PostData/5
+        [ResponseType(typeof(Post))]
+        public IHttpActionResult DeletePost(int id)
+        {
+            Post post = db.Posts.Find(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            db.Posts.Remove(post);
+            db.SaveChanges();
+
+            return Ok(post);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool PostExists(int id)
+        {
+            return db.Posts.Count(e => e.PostID == id) > 0;
+        }
+    }
+}
--- HospitalApplication/Models/Post.cs
+++ HospitalApplication/Models/Post.cs
@@ -10,15 +10,15 @@ namespace HospitalApplication.Models
     public class Post
     {
         [Key]
-        public int PostId { get; set; }
+        public int PostID { get; set; }
         public string Title { get; set; }
         public DateTime DateCreated { get; set; }
         public string Content { get; set; }
-        
+
         //A post belongs to one user
         //A user can create many posts
-        [ForeignKey("User")]
-        public string UserId { get; set; }
-        public virtual ApplicationUser User { get; set; }
+        [ForeignKey("ApplicationUser")]
+        public string UserID { get; set; }
+        public virtual ApplicationUser ApplicationUser { get; set; }
     }
 }
