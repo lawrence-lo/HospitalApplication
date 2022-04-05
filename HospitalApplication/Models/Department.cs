f75561610550e2d838c36e93f25b0cae1fa68fde
aaronhpchan <aaronhpchan@gmail.com>
2022-04-05 16:59:35 -0400

    Update Department model

--- HospitalApplication/Models/Department.cs
+++ HospitalApplication/Models/Department.cs
@@ -18,9 +18,9 @@ namespace HospitalApplication.Models
         /* To-do: Dept Head, Staff No. */
 
         //A department can receive many donations
-        //A department can have many jobs
+        //A department can have many users
         public ICollection<Donation> Donations { get; set; }
-        public ICollection<Job> Jobs { get; set; }
+        public ICollection<UserDto> Users { get; set; }
     }
 
     public class DepartmentDto
