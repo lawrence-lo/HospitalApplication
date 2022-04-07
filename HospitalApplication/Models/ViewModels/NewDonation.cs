a67ffa884362a6ac90f8db0cbf8be7ab2b14a960
nayer-rsl <nayer.rsl@gmail.com>
2022-04-07 17:58:37 -0400

    Add Donation Viewmodel

---
+++ HospitalApplication/Models/ViewModels/NewDonation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HospitalApplication.Models.ViewModels
+{
+    public class NewDonation
+    {
+        public DonationDto DonationDto { get; set; }
+
+        //list of Donors
+        public IEnumerable<DonorDto> DonorOptions { get; set; }
+        public IEnumerable<DepartmentDto> DepartmentOptions { get; set; }
+    }
+}
